daf66bfadd7d9736ed24785b67baff2a6020ab12
Gregory Rogachev <kotsmilewtm@gmail.com>
2024-02-07 22:34:25 +0100

    move tasks to separate file

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,20 @@
 use std::sync::Arc;
 
 use anyhow::{Context, Result};
-use crons::rpc_feed_cron;
-use rocket::tokio::{sync::RwLock, task};
+use rocket::tokio::sync::RwLock;
 
 mod client;
 mod controllers;
 mod crons;
 mod middleware;
+
 mod models;
 mod repo;
 mod services;
+
 mod setup;
+mod tasks;
+
 mod util;
 
 use client::{
@@ -21,75 +24,11 @@ use client::{
 };
 use repo::{cache::CacheRepo, config::ConfigRepo, storage::StorageRepo};
 use services::{
-    evm_rpc::{EvmRpcService, RpcMetrics},
-    jwt::JwtService,
-    monitoring::MonitoringService,
-    proxy::ProxyService,
+    evm_rpc::EvmRpcService, jwt::JwtService, monitoring::MonitoringService, proxy::ProxyService,
     user::UserService,
 };
 use setup::setup_app;
-
-async fn run_tasks(
-    evm_rpc_service: Arc<EvmRpcService>,
-    proxy_service: Arc<RwLock<ProxyService>>,
-    config_repo: ConfigRepo,
-) {
-    {
-        let proxy_service = proxy_service.clone();
-        task::spawn(async move {
-            let _ = proxy_service
-                .write()
-                .await
-                .init_proxies()
-                .await
-                .map_err(|err| log::error!("failed to init proxy: {err}"));
-        });
-    }
-
-    {
-        let evm_rpc_service = evm_rpc_service.clone();
-        let proxy_service = proxy_service.clone();
-        let supported_chain_ids = config_repo.supported_chain_ids.clone();
-        let feed_max_timeout = config_repo.feed_max_timeout.clone();
-        task::spawn(async move {
-            rpc_feed_cron(
-                evm_rpc_service,
-                proxy_service,
-                supported_chain_ids,
-                feed_max_timeout,
-            )
-            .await;
-        });
-    }
-
-    let chain_to_rpc = evm_rpc_service
-        .fetch_rpcs()
-        .await
-        .map_err(|err| log::error!("failed to fetch rpcs from chainlist: {err}"));
-    let Ok(chain_to_rpc) = chain_to_rpc else {
-        return;
-    };
-
-    for chain_id in &config_repo.supported_chain_ids {
-        let Some(rpcs) = chain_to_rpc.get(chain_id) else {
-            continue;
-        };
-
-        let mut rpcs_for_chain_id: Vec<(String, RpcMetrics)> = Vec::new();
-        for rpc in rpcs {
-            rpcs_for_chain_id.push((
-                rpc.clone(),
-                RpcMetrics {
-                    response_time_ms: 0,
-                },
-            ));
-        }
-
-        evm_rpc_service
-            .set_rpcs_for_chain_id(chain_id, rpcs_for_chain_id)
-            .await;
-    }
-}
+use tasks::run_tasks;
 
 #[rocket::main]
 async fn main() -> Result<()> {
---
+++ src/tasks.rs
@@ -0,0 +1,74 @@
+use std::sync::Arc;
+
+use rocket::tokio::{sync::RwLock, task};
+
+use crate::{
+    crons::rpc_feed_cron,
+    repo::config::ConfigRepo,
+    services::{
+        evm_rpc::{EvmRpcService, RpcMetrics},
+        proxy::ProxyService,
+    },
+};
+
+pub async fn run_tasks(
+    evm_rpc_service: Arc<EvmRpcService>,
+    proxy_service: Arc<RwLock<ProxyService>>,
+    config_repo: ConfigRepo,
+) {
+    {
+        let proxy_service = proxy_service.clone();
+        task::spawn(async move {
+            let _ = proxy_service
+                .write()
+                .await
+                .init_proxies()
+                .await
+                .map_err(|err| log::error!("failed to init proxy: {err}"));
+        });
+    }
+
+    {
+        let evm_rpc_service = evm_rpc_service.clone();
+        let proxy_service = proxy_service.clone();
+        let supported_chain_ids = config_repo.supported_chain_ids.clone();
+        let feed_max_timeout = config_repo.feed_max_timeout.clone();
+        task::spawn(async move {
+            rpc_feed_cron(
+                evm_rpc_service,
+                proxy_service,
+                supported_chain_ids,
+                feed_max_timeout,
+            )
+            .await;
+        });
+    }
+
+    let chain_to_rpc = evm_rpc_service
+        .fetch_rpcs()
+        .await
+        .map_err(|err| log::error!("failed to fetch rpcs from chainlist: {err}"));
+    let Ok(chain_to_rpc) = chain_to_rpc else {
+        return;
+    };
+
+    for chain_id in &config_repo.supported_chain_ids {
+        let Some(rpcs) = chain_to_rpc.get(chain_id) else {
+            continue;
+        };
+
+        let mut rpcs_for_chain_id: Vec<(String, RpcMetrics)> = Vec::new();
+        for rpc in rpcs {
+            rpcs_for_chain_id.push((
+                rpc.clone(),
+                RpcMetrics {
+                    response_time_ms: 0,
+                },
+            ));
+        }
+
+        evm_rpc_service
+            .set_rpcs_for_chain_id(chain_id, rpcs_for_chain_id)
+            .await;
+    }
+}
